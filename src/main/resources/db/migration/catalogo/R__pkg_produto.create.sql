0ac1d02dee6f5fdc68eda93279c4dac74175998d
Jean Nascimento <jeancnasc@hotmail.com>
2022-05-06 22:36:54 -0300

    commit inicial

---
+++ src/main/resources/db/migration/catalogo/R__pkg_produto.create.sql
@@ -0,0 +1,11 @@
+--Create a new Package
+
+CREATE OR REPLACE PACKAGE CATALOGO.PKG_PRODUTO IS
+
+  -- Add a procedure
+  PROCEDURE INSERIR (
+    NOME VARCHAR, 
+    MODELO VARCHAR);
+
+END PKG_PRODUTO;
+/
---
+++ src/main/resources/db/migration/catalogo/V2__produto.create.sql
@@ -0,0 +1,5 @@
+create table catalogo.produto (
+    ID NUMBER(10) NOT NULL,
+    NOME VARCHAR(100) NOT NULL,
+    MODELO VARCHAR(100)
+);
